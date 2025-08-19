7acf76abbe30e67e7ea8f0aad0d763a3121ed66f
Casper Boone <mail@casperboone.nl>
2025-08-19 17:49:04 +0200

    Change Bruno to Dante

--- components/HomoBingo.js
+++ components/HomoBingo.js
@@ -6,9 +6,9 @@ export default function HomoBingo() {
       <img src={HomoBingoImage.src} className="h-56 w-full object-cover" />
       <div className="p-4">
         <strong>Hoihoi!</strong> Superleuk dat je bij de <strong>homobingo</strong> bent, you're in for a treat want dit is niet je standaard bingo 👀
-        Al jaren wordt de homobingo gepresenteerd door de fabuleuze <strong>ZsaZsa</strong> en haar Duitse assistent <strong>Bruno</strong>.
+        Al jaren wordt de homobingo gepresenteerd door de fabuleuze <strong>ZsaZsa</strong> en haar assistent <strong>Dante</strong>.
         Zoals gewoonlijk zijn er prijzen te winnen voor een goede bingo, maar roep pas <strong>BINGO</strong> als je het zeker weet!
-        ZsaZsa en Bruno zijn heel zachtaardig maar van een <strong>valse bingo</strong> worden zij ook vals!
+        ZsaZsa en Dante zijn heel zachtaardig maar van een <strong>valse bingo</strong> worden zij ook vals!
       </div>
     </div>
   )
